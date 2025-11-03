bb38646243e89c1b5e0c3cb676cfef957ad71548
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2025-11-03 16:58:54 -0300

    projeto do dia 03/11/2025

---
+++ aula05/Introducao.ts
@@ -0,0 +1,40 @@
+console.log("Hello World!");
+
+// importa o módulo
+
+import leia = require("readline-sync");
+
+// Declaração de variáveis
+let quantidade: number;
+let altura: number;
+let tipo: string;
+let resposta: boolean;
+let palavra: string;
+
+quantidade = leia.questionInt("\nDigite um valor do tipo Inteiro: ", {
+  limitMessage: "Digite um número inteiro",
+});
+console.log("O valor inteiro digitado foi: " + quantidade);
+
+altura = leia.questionFloat("\nDigite um valor do tipo decimal: ", {
+  limitMessage: "Digite um número decimal",
+});
+console.log("O valor decimal digitado foi: " + altura);
+
+console.log("\nDigite um valor do tipo Char - um único caractere: ");
+tipo = leia.keyIn();
+console.log("O valor char digitado foi: " + tipo);
+
+resposta = leia.keyInYNStrict("\nDigite um valor do tipo Boolean (S/N): ");
+console.log("O valor boolean digitado foi: " + resposta);
+
+palavra = leia.question("\nDigite um valor do tipo string: ");
+console.log("O valor string digitado foi: " + palavra);
+
+// Exibe os tipos das variáveis
+console.log("\nTipos das variáveis:");
+console.log("quantidade:", typeof quantidade);
+console.log("altura:", typeof altura);
+console.log("tipo:", typeof tipo);
+console.log("resposta:", typeof resposta);
+console.log("palavra:", typeof palavra);
