59f549449b77a50d0af92a64472c2f4acf8428fa
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2025-11-12 11:17:38 -0300

    aula14

---
+++ aula14/Exemplo1.ts
@@ -0,0 +1,30 @@
+export function main() {
+  let numeros: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  console.log("Exibir os itens\n");
+
+  // Exibir os itens da Lista sem Arrow Functions
+  for (let numero of numeros) {
+    console.log(numero);
+  }
+
+  console.log("\nExibir os itens somados com eles mesmos\n");
+
+  // Exibir os itens da Lista dobrados com Arrow Functions
+  //   for (let numero of numeros) {
+  //     console.log(numero + numero);
+  //   }
+  //   numeros.forEach(function (numero: number) {
+  //     console.log(numero);
+  //   });
+
+  numeros.forEach((numero) => console.log(numero));
+
+  console.log("\nExibir os itens pares da lista\n");
+
+  // Exibir apenas os elementos pares da Lista sem Arrow Functions
+  for (let numero of numeros) {
+    if (numero % 2 == 0) console.log(numero);
+  }
+}
+main();
